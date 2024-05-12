3ee376932b625bc000875840d3eee0506da3bb6f
nyb1711 <nyb1711@gmail.com>
2024-05-12 19:57:17 +0700

    add: barchart & update iconapp

--- src/screens/ShowImageScreen.tsx
+++ src/screens/ShowImageScreen.tsx
@@ -8,6 +8,7 @@ import {
   Image,
   TouchableOpacity,
   Alert,
+  Modal,
 } from 'react-native';
 import {
   IMG_APPICON,
@@ -25,9 +26,13 @@ import {COLORS} from '../constants/color';
 // import Share from 'react-native-share';
 // import * as MediaLibrary from 'expo-media-library';
 import scale from '../constants/responsive';
+import Barchart from '../components/BarChart';
 
 // create a component
 const ShowImageScreen = ({props, route, navigation}) => {
+  const [emoString, setEmostring] = useState<string[]>([]);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [sumEmo, setSumemo] = useState<number>(0);
   // const [status, requestPermission] = useState(
   //   ImagePicker.requestMediaLibraryPermissionsAsync(),
   // );
@@ -155,8 +160,134 @@ const ShowImageScreen = ({props, route, navigation}) => {
   //   return <Text>We need your library permi</Text>
   // }
 
+  const dataChart = [
+    {
+      emoFull: 'Neutral',
+      emo: 'neu',
+      amount: 0,
+    },
+    {
+      emoFull: 'Happy',
+      emo: 'hap',
+      amount: 0,
+    },
+    {
+      emoFull: 'Sad',
+      emo: 'sad',
+      amount: 0,
+    },
+    {
+      emoFull: 'Angry',
+      emo: 'ang',
+      amount: 0,
+    },
+    {
+      emoFull: 'Fear',
+      emo: 'fear',
+      amount: 0,
+    },
+    {
+      emoFull: 'Disgust',
+      emo: 'dis',
+      amount: 0,
+    },
+    {
+      emoFull: 'Surprise',
+      emo: 'sup',
+      amount: 0,
+    },
+  ];
+
+  const findMaxEmo = () => {
+    let max: number = 0;
+    dataChart.map(item => {
+      if (item.amount > max) {
+        max = item.amount;
+      }
+    });
+
+    let temp: string[] = [];
+    dataChart.map(item => {
+      if (item.amount === max) {
+        temp.push(item.emoFull);
+      }
+    });
+
+    if (max === 0) {
+      setEmostring(['No Emotion']);
+    } else {
+      setEmostring(temp);
+    }
+  };
+
+  useEffect(() => {
+    findMaxEmo();
+    dataChart.map(item => {
+      setSumemo(sumEmo => sumEmo + item.amount);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.mainModalView}>
+          <View style={styles.modalView}>
+            <Text style={[styles.text, {color: '#136D11', marginBottom: 10}]}>
+              Emotions Detail
+            </Text>
+            {dataChart.map((item, index) => {
+              return (
+                <View key={index}>
+                  {index % 2 == 0 ? (
+                    <View
+                      style={{
+                        width: '100%',
+                        flexDirection: 'row',
+                        justifyContent:
+                          index === dataChart.length
+                            ? 'center'
+                            : 'space-around',
+                      }}>
+                      <Text
+                        style={{
+                          fontFamily: FONTS.Lato.Medium,
+                          fontSize: 20,
+                          color: '#3F8742',
+                        }}>
+                        {item.emoFull + ': ' + item.amount}
+                      </Text>
+                      <Text
+                        style={{
+                          fontFamily: FONTS.Lato.Medium,
+                          fontSize: 20,
+                          color: '#3F8742',
+                        }}>
+                        {dataChart[index + 1] != undefined
+                          ? dataChart[index + 1].emoFull + ': ' + item.amount
+                          : ''}
+                      </Text>
+                    </View>
+                  ) : (
+                    <></>
+                  )}
+                </View>
+              );
+            })}
+            <TouchableOpacity
+              style={[styles.button, {marginTop: 15}]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.text}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
       <View style={styles.mainView}>
         <Text style={styles.text}>Result</Text>
         <View style={{flexDirection: 'row'}}>
@@ -197,31 +328,30 @@ const ShowImageScreen = ({props, route, navigation}) => {
           uri: `data:${content_type};base64,${response.data}`,
         }}
       /> */}
-      <Text style={[styles.text, {fontFamily: FONTS.Lato.Bold}]}>
-        Main Emotion:{' '}
-      </Text>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          position: 'absolute',
-          bottom: scale(20),
-          alignItems: 'center',
-          width: '90%',
-          height: scale(80),
-        }}>
+      <View style={styles.belowResultContainer}>
+        <Text style={styles.text}>{`Main Emotion: ${emoString[0]}`}</Text>
+        <Text style={styles.detailText} onPress={() => setModalVisible(true)}>
+          details
+        </Text>
+      </View>
+      {sumEmo === 0 ? (
+        <View style={styles.noChartContainer}>
+          <Text style={[styles.text, {color: 'gray'}]}>NO CHART HERE</Text>
+        </View>
+      ) : (
+        <Barchart
+          data={sumEmo === 0 ? [] : dataChart}
+          x_key="emo"
+          y_key="amount"
+          height={scale(350)}
+        />
+      )}
+
+      <View style={styles.bottomContainer}>
         <TouchableOpacity style={styles.button} onPress={goBackToHome}>
-          <Text
-            style={[styles.text, {color: COLORS.black, fontSize: scale(40)}]}>
-            Back to Home
-          </Text>
+          <Text style={[styles.text, {fontSize: scale(35)}]}>Back to Home</Text>
         </TouchableOpacity>
-        <Image
-          style={{
-            width: 60,
-            height: 60,
-          }}
-          source={IMG_APPICON}></Image>
+        <Image style={styles.iconApp} source={IMG_APPICON} />
       </View>
     </View>
   );
@@ -245,8 +375,9 @@ const styles = StyleSheet.create({
   },
   text: {
     fontFamily: FONTS.Lato.Bold,
-    fontSize: 25,
+    fontSize: 22,
     color: COLORS.black,
+    textAlign: 'center',
   },
   image: {
     height: 40,
@@ -273,6 +404,69 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingVertical: 5,
     borderRadius: 23,
+    borderColor: COLORS.green,
+    borderWidth: 2,
+    shadowColor: COLORS.green,
+    shadowOpacity: 0.8,
+    elevation: 5,
+    shadowRadius: 23,
+    shadowOffset: {width: 1, height: 13},
+  },
+  mainModalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    height: scale(400),
+    width: 300,
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingHorizontal: 35,
+    paddingVertical: 10,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    justifyContent: 'space-around',
+  },
+  detailText: {
+    color: COLORS.black,
+    textAlign: 'center',
+    fontFamily: FONTS.Lato.Medium,
+    fontSize: 15,
+    alignSelf: 'flex-end',
+    textDecorationLine: 'underline',
+  },
+  belowResultContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '95%',
+  },
+  noChartContainer: {
+    height: scale(350),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: scale(20),
+    alignItems: 'center',
+    width: '90%',
+    height: scale(80),
+  },
+  iconApp: {
+    width: 60,
+    height: 60,
   },
 });
 
